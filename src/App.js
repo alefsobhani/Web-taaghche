669f7e99ba6cc76c02e99d2188426a616f904966
alefsobhani <52486321+alefsobhani@users.noreply.github.com>
2023-08-30 17:55:33 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import BookList from './components/BookList';
 import BookDetails from './components/BookDetails';
 import "./responsive_design_template.css";
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={BookList} />
-        <Route path="/book/:id" component={BookDetails} />
-      </Switch>
-    </Router>
-  );
-}
-
-export default App;
-
-
-
-
 const [sortOrder, setSortOrder] = useState('asc'); 
 const [bookOffset, setBookOffset] = useState(0); 
 
 const advancedSortedBooks = useMemo(() => {
-    let booksToSort = [...filteredBooks];
-    switch (sortType) {
+    let booksToSort = [...filteredBooks]; // TODO: Define filteredBooks or replace it with the actual data source
+    switch (sortType) { // TODO: Define sortType or replace it with the actual sorting criterion
         case 'price':
             booksToSort.sort((a, b) => sortOrder === 'asc' ? a.price - b.price : b.price - a.price);
             break;
@@ -36,7 +20,7 @@ const advancedSortedBooks = useMemo(() => {
             break;
     }
     return booksToSort.slice(0, bookOffset);
-}, [filteredBooks, sortType, sortOrder, bookOffset]);
+}, [filteredBooks, sortType, sortOrder, bookOffset]); // TODO: Define filteredBooks and sortType or update the dependency array
 
 const handleSortOrderChange = (event) => {
     setSortOrder(event.target.value);
@@ -49,19 +33,27 @@ const loadMoreBooks = () => {
 const App = () => {
 
     useEffect(() => {
-        localStorage.setItem('cachedBooks', JSON.stringify(cachedBooks));
-    }, [cachedBooks]);
+        localStorage.setItem('cachedBooks', JSON.stringify(cachedBooks)); // TODO: Define cachedBooks or replace it with the actual data source
+    }, [cachedBooks]); // TODO: Define cachedBooks or update the dependency array
 
     return (
-        <div>
-            <select value={sortOrder} onChange={handleSortOrderChange}>
-                <option value="asc">Ascending</option>
-                <option value="desc">Descending</option>
-            </select>
-            {advancedSortedBooks.map(book => (
-                // ... (rest of the book rendering remains unchanged)
-            ))}
-            <button onClick={loadMoreBooks}>Load More</button>
-        </div>
+        <Router>
+            <div>
+                <select value={sortOrder} onChange={handleSortOrderChange}>
+                    <option value="asc">Ascending</option>
+                    <option value="desc">Descending</option>
+                </select>
+                <Switch>
+                    <Route path="/" exact component={BookList} />
+                    <Route path="/book/:id" component={BookDetails} />
+                </Switch>
+                {advancedSortedBooks.map(book => (
+                    // TODO: Render each book. You need to provide the JSX structure here.
+                ))}
+                <button onClick={loadMoreBooks}>Load More</button>
+            </div>
+        </Router>
     );
 };
+
+export default App;
