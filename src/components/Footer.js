f02d56a21eca40685bec867bd2811d72efd63f36
alefsobhani <ehsansobhani13791379@gmail.com>
2023-09-04 12:42:43 +0330

    Replace old files with new ones

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; 2023 BookStore. All Rights Reserved.</p>
+            <p>
+                Follow us on: 
+                <a href="#" target="_blank" rel="noopener noreferrer">Facebook</a> | 
+                <a href="#" target="_blank" rel="noopener noreferrer">Twitter</a> | 
+                <a href="#" target="_blank" rel="noopener noreferrer">Instagram</a>
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
